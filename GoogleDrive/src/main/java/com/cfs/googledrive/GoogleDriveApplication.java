996eff2e1fb4f9323561980bba506acd55d2c3d1
Mohit Pawar <mohitrpawar0061@gmail.com>
2026-07-04 18:58:49 +0530

    Add all microservices

---
+++ GoogleDrive/src/main/java/com/cfs/googledrive/GoogleDriveApplication.java
@@ -0,0 +1,14 @@
+package com.cfs.googledrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GoogleDriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleDriveApplication.class, args);
+    }
+
+}
